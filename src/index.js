be29a36b65f9c67785dbdf19c9fd7347625eec76
roopxx <127219908+roopxx@users.noreply.github.com>
2024-01-03 14:51:49 +0530

    Add home section and responsive layout

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ const navButton = document.querySelector(".nav_button");
 const navList = document.querySelector(".nav_list");
 
 navButton.addEventListener("click", () => {
+  navButton.firstElementChild.classList.toggle("hidden");
+  navButton.lastElementChild.classList.toggle("hidden");
   navList.classList.toggle("hidden");
 });
