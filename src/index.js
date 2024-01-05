ab58cc94b49df910b984e53e89d648e295453177
roopxx <127219908+roopxx@users.noreply.github.com>
2024-01-05 14:08:30 +0530

    fix Home, About & Projects links

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import "./style.css";
 
 const navButton = document.querySelector(".nav_button");
 const navList = document.querySelector(".nav_list");
+const navLink = document.querySelectorAll(".nav_link");
 
 navButton.addEventListener("click", () => {
   navButton.firstElementChild.classList.toggle("hidden");
   navButton.lastElementChild.classList.toggle("hidden");
   navList.classList.toggle("hidden");
 });
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    navList.classList.toggle("hidden");
+    navButton.firstElementChild.classList.toggle("hidden");
+    navButton.lastElementChild.classList.toggle("hidden");
+  })
+);
