47b94aab0501d867b33ab6d8d2920376dbbc5525
roopxx <127219908+roopxx@users.noreply.github.com>
2024-01-06 15:04:51 +0530

    feat: add custom mouse hover effect

--- src/index.js
+++ src/index.js
@@ -17,3 +17,39 @@ navLink.forEach((link) =>
     navButton.lastElementChild.classList.toggle("hidden");
   })
 );
+
+const circle = document.querySelector(".circle");
+
+const hovers = document.querySelectorAll(".this");
+
+hovers.forEach((hover) => {
+  hover.addEventListener("mouseover", () => {
+    circle.classList.add("enlarge");
+  });
+
+  hover.addEventListener("mouseout", () => {
+    circle.classList.remove("enlarge");
+  });
+});
+
+window.addEventListener("mousemove", function (e) {
+  circle.style.left = e.clientX + "px";
+  circle.style.top = e.clientY + "px";
+});
+
+const nonHovers = document.querySelectorAll(".tech-stack");
+
+nonHovers.forEach((noHover) => {
+  noHover.addEventListener("mouseenter", () => {
+    circle.classList.remove("circle");
+  });
+
+  noHover.addEventListener("mouseleave", () => {
+    circle.classList.add("circle");
+  });
+});
+
+window.addEventListener("mousemove", function (e) {
+  circle.style.left = e.clientX + "px";
+  circle.style.top = e.clientY + "px";
+});
